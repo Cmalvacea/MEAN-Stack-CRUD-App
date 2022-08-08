de546e79cac006385e90981565fbac94efcf64ee
Christopher Malvacea <christophermalvacea@gmail.com>
2022-08-08 08:57:44 -0400

    Basic CRUD control
    Basic CRUD controller is finished
    
    - Added a GetTasks function
    - Added two UpdateTask function
    . Added one DeleteTask function

--- Backend/DB_Controller.cs
+++ Backend/DB_Controller.cs
@@ -14,5 +14,41 @@ namespace DBControl {
         };
         Collection.InsertOne(Task);
     }
+
+    public void GetTasksU() {
+
+        var Client = new MongoClient("mongodb://localhost:27017");
+        var DataB = Client.GetDatabase("D-NET-DB");
+        var Collection = DataB.GetCollection<BsonDocument>("Tasks");
+        var DocumentList = Collection.Find(new BsonDocument()).ToList();
+        System.Console.WriteLine(DocumentList);
+
+    }
+
+    public void UpdateTaskForName(string OldName, string NewName) {
+        var Client = new MongoClient("mongodb://localhost:27017");
+        var DataB = Client.GetDatabase("D-NET-DB");
+        var Collection = DataB.GetCollection<BsonDocument>("Tasks");
+        var Filter = Builders<BsonDocument>.Filter.Eq("Task_Name", OldName);
+        var N_Update = Builders<BsonDocument>.Update.Set("Task_Name", NewName);
+        Collection.UpdateOne(Filter, N_Update);
+    }
+
+    public void UpdateTaskForDetails(string OldName, string NewDetails) {
+        var Client = new MongoClient("mongodb://localhost:27017");
+        var DataB = Client.GetDatabase("D-NET-DB");
+        var Collection = DataB.GetCollection<BsonDocument>("Tasks");
+        var Filter = Builders<BsonDocument>.Filter.Eq("Task_Name", OldName);
+        var D_Update = Builders<BsonDocument>.Update.Set("Task_Details", NewDetails);
+        Collection.UpdateOne(Filter, D_Update)
+    }
+
+    public void DeleteTask(string OldName) {
+        var Client = new MongoClient("mongodb://localhost:27017");
+        var DataB = Client.GetDatabase("D-NET-DB");
+        var Collection = DataB.GetCollection<BsonDocument>("Tasks");
+        var Filter = Builders<BsonDocument>.Filter.Eq("Task_Name", OldName);
+        Collection.DeleteOne(Filter);
+    }
 };
 }
