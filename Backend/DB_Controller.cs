0b767a3765267009433d48564c1ca2172cffe6f2
Christopher Malvacea <christophermalvacea@gmail.com>
2022-08-09 14:56:27 -0400

    Almost Done!
    - API endpoints added for creation, elimination and updates
    - Front-end started and almost finished
    
    Pending:
    - Angular addition
    - Endpoint for getting tasks

--- Backend/DB_Controller.cs
+++ Backend/DB_Controller.cs
@@ -1,10 +1,13 @@
 using MongoDB.Bson;
 using MongoDB.Driver;
+using Microsoft.OpenApi.Exceptions;
+using Microsoft.OpenApi.Validations;
 
 
 namespace DBControl {
     class CRUD_Control {
     public void CreateTask(string Name, string Details) {
+        var Success = (string Mg) => Mg;
         var Client = new MongoClient("mongodb://localhost:27017");
         var DataB = Client.GetDatabase("D-NET-DB");
         var Collection = DataB.GetCollection<BsonDocument>("Tasks");
@@ -12,15 +15,22 @@ namespace DBControl {
             {"Task_Name", Name},
             {"Task_Details", Details}
         };
-        Collection.InsertOne(Task);
+        var filter = Builders<BsonDocument>.Filter.Eq("Task_Name", Name);
+        var Check = Collection.Find(filter);
+        var Check_Count = Check.CountDocuments();
+        int CorrectCount = (int) Check_Count;
+        if(CorrectCount >= 1) {
+            throw new InvalidOperationException("There is already a task with this name");
+        } else {
+            Collection.InsertOne(Task);
+        }
     }
 
-    public void GetTasksU() {
-
+    public void GetTasks() {
         var Client = new MongoClient("mongodb://localhost:27017");
         var DataB = Client.GetDatabase("D-NET-DB");
         var Collection = DataB.GetCollection<BsonDocument>("Tasks");
-        var DocumentList = Collection.Find(new BsonDocument()).ToList();
+        var DocumentList = Collection.Find(new BsonDocument()).ToListAsync();
         System.Console.WriteLine(DocumentList);
 
     }
@@ -31,7 +41,12 @@ namespace DBControl {
         var Collection = DataB.GetCollection<BsonDocument>("Tasks");
         var Filter = Builders<BsonDocument>.Filter.Eq("Task_Name", OldName);
         var N_Update = Builders<BsonDocument>.Update.Set("Task_Name", NewName);
-        Collection.UpdateOne(Filter, N_Update);
+        var Query = Collection.Find(Filter).CountDocuments();
+        if(Query == 0) {
+            throw new InvalidOperationException("This task does not exist");
+        } else {
+            Collection.UpdateOne(Filter, N_Update);
+        }
     }
 
     public void UpdateTaskForDetails(string OldName, string NewDetails) {
@@ -40,7 +55,12 @@ namespace DBControl {
         var Collection = DataB.GetCollection<BsonDocument>("Tasks");
         var Filter = Builders<BsonDocument>.Filter.Eq("Task_Name", OldName);
         var D_Update = Builders<BsonDocument>.Update.Set("Task_Details", NewDetails);
-        Collection.UpdateOne(Filter, D_Update)
+        var Query = Collection.Find(Filter).CountDocuments();
+        if(Query == 0) {
+            throw new InvalidOperationException("This task does not exist");
+        } else {
+            Collection.UpdateOne(Filter, D_Update);
+        }
     }
 
     public void DeleteTask(string OldName) {
@@ -48,7 +68,12 @@ namespace DBControl {
         var DataB = Client.GetDatabase("D-NET-DB");
         var Collection = DataB.GetCollection<BsonDocument>("Tasks");
         var Filter = Builders<BsonDocument>.Filter.Eq("Task_Name", OldName);
-        Collection.DeleteOne(Filter);
+        var Query = Collection.Find(Filter).CountDocuments();
+        if(Query == 0) {
+            throw new InvalidOperationException("This task does not exist");
+        } else {
+            Collection.DeleteOne(Filter);
+        }
     }
 };
 }
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -4,28 +4,67 @@ using DBControl;
 var builder = WebApplication.CreateBuilder(args);
 var NewController = new CRUD_Control();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRUD Angular/.NET", Description = "An odd CRUD for showcase", Version = "v1" });
-});
-
-
 
 var app = builder.Build();
 
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-   c.SwaggerEndpoint("/swagger/v1/swagger.json", ".NET Minimal API v1");
+
+
+
+
+
+app.MapPost("/Task/{Name}/{Details}", (String Name, String Details) => {
+    try
+    {
+        NewController.CreateTask(Name, Details);
+    }
+    catch (System.Exception ex)
+    {
+        var ThrowFail = () => $"{ex}";
+    }
+    finally {
+        Results.Text("Task created succesfully");
+    }
 });
-
-NewController.CreateTask("Task 1", "Task 1 details");
-
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/ArgTest/{id}", (String id) => "Hello " + id);
+app.MapDelete("/Task/Delete/{name}", (string name) => {
+    try
+    {
+        NewController.DeleteTask(name);
+    }
+    catch (System.Exception ex)
+    {
+        var ThrowFail = () => $"{ex}";
+    }
+    finally {
+        Results.Text("Task deleted succesfully");
+    }
+});
+app.MapPut("/Task/UpdateName/{name}/{newName}", (string name, string NewName) => {
+    try
+    {
+        NewController.UpdateTaskForName(name, NewName);
+    }
+    catch (System.Exception ex)
+    {
+        var ThrowFail = () => $"{ex}";
+    }
+    finally {
+        Results.Text("Task updated succesfully");
+    }
+});
+app.MapPut("/Task/UpdateDetails/{name}/{newDetails}", (string name, string NewDetails) => {
+    try
+    {
+        NewController.UpdateTaskForDetails(name, NewDetails);
+    }
+    catch (System.Exception ex)
+    {
+        var ThrowFail = () => $"{ex}";
+    }
+    finally {
+        Results.Text("Task updated succesfully");
+    }
+});
 
 app.Run();
 
